e0ad936b20947fc955aca3bbe685e84799c15611
SwimmingMantarain <SwimmingMantarain@gmail.com>
2024-12-03 16:54:08 +0000

    multiboot2? idk

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cargo build
+mkdir -p iso/boot/grub
+cp target/x86_64-none-bare_metal/debug/Kice-OS iso/boot/kernel.elf
+grub-mkrescue -o kice-os.iso iso 2> /dev/null
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+core::arch::global_asm!(include_str!("start.asm"));
+core::arch::global_asm!(include_str!("multiboot2_header.asm"));
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+fn entry_rust() {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
