270fbac4e90bf7acd99dc2fd1e7f98899f00ba0b
SwimmingMantarain <SwimmingMantarain@gmail.com>
2024-12-04 16:08:18 +0000

    probably not like this, interrupts first

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,28 @@ core::arch::global_asm!(include_str!("multiboot2_header.asm"));
 
 use core::panic::PanicInfo;
 
+#[macro_use]
+mod vga_buffer;
+
 #[no_mangle]
 fn entry_rust() {
-    loop {}
+    println!("Hello, world!");
+
+    loop {
+        hlt_loop();
+    }
+}
+
+fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    println!("{}", _info);
+    loop {
+        hlt_loop();
+    }
 }
